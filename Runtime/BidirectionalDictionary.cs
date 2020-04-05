da95560d4a5138218daac13c85e1f3a32d0b6b60
Taro Omiya <taro.omiya@omiyagames.com>
2020-04-04 22:50:48 -0400

    Issue #3: started working on implementing all the common constructors in the BidirectionalDictionary. Also, I confess this log is to get Github to detect this message.

--- Runtime/BidirectionalDictionary.cs
+++ Runtime/BidirectionalDictionary.cs
@@ -49,79 +49,134 @@ namespace OmiyaGames
     ///   </item>
     /// </list>
     /// </remarks>
-
     public class BidirectionalDictionary<KEY, VALUE> : IDictionary<KEY, VALUE>
     {
-        readonly Dictionary<KEY, VALUE> keyToValueMap;
-        readonly Dictionary<VALUE, KEY> valueToKeyMap;
+        /// <summary>
+        /// The dictionary mapping from key to value.
+        /// </summary>
+        /// <seealso cref="ValueToKeyMap"/>
+        private Dictionary<KEY, VALUE> KeyToValueMap
+        {
+            get;
+        }
+        /// <summary>
+        /// The dictionary mapping from value to key.
+        /// </summary>
+        /// <seealso cref="KeyToValueMap"/>
+        private Dictionary<VALUE, KEY> ValueToKeyMap
+        {
+            get;
+        }
+
+        #region Constructors
+        public BidirectionalDictionary()
+        {
+            KeyToValueMap = new Dictionary<KEY, VALUE>();
+            ValueToKeyMap = new Dictionary<VALUE, KEY>();
+        }
+
+        public BidirectionalDictionary(int capacity)
+        {
+            KeyToValueMap = new Dictionary<KEY, VALUE>(capacity);
+            ValueToKeyMap = new Dictionary<VALUE, KEY>(capacity);
+        }
+
+        public BidirectionalDictionary(IEqualityComparer<KEY> keyComparer, IEqualityComparer<VALUE> valueComparer)
+        {
+            KeyToValueMap = new Dictionary<KEY, VALUE>(keyComparer);
+            ValueToKeyMap = new Dictionary<VALUE, KEY>(valueComparer);
+        }
+
+        public BidirectionalDictionary(int capacity, IEqualityComparer<KEY> keyComparer, IEqualityComparer<VALUE> valueComparer)
+        {
+            KeyToValueMap = new Dictionary<KEY, VALUE>(capacity, keyComparer);
+            ValueToKeyMap = new Dictionary<VALUE, KEY>(capacity, valueComparer);
+        }
+
+        public BidirectionalDictionary(IDictionary<KEY, VALUE> dictionary) : this()
+        {
+            foreach(KeyValuePair<KEY, VALUE> pair in dictionary)
+            {
+                Add(pair);
+            }
+        }
+
+        public BidirectionalDictionary(IDictionary<KEY, VALUE> dictionary, IEqualityComparer<KEY> keyComparer, IEqualityComparer<VALUE> valueComparer) : this(keyComparer, valueComparer)
+        {
+            foreach (KeyValuePair<KEY, VALUE> pair in dictionary)
+            {
+                Add(pair);
+            }
+        }
+        #endregion
 
         public VALUE this[KEY key]
         {
-            get => keyToValueMap[key];
-            set => keyToValueMap[key] = value;
+            get => KeyToValueMap[key];
+            set => KeyToValueMap[key] = value;
         }
 
-        public ICollection<KEY> Keys => ((IDictionary<KEY, VALUE>)keyToValueMap).Keys;
+        public ICollection<KEY> Keys => KeyToValueMap.Keys;
 
-        public ICollection<VALUE> Values => ((IDictionary<KEY, VALUE>)keyToValueMap).Values;
+        public ICollection<VALUE> Values => KeyToValueMap.Values;
 
-        public int Count => keyToValueMap.Count;
+        public int Count => KeyToValueMap.Count;
 
-        public bool IsReadOnly => ((IDictionary<KEY, VALUE>)keyToValueMap).IsReadOnly;
+        public bool IsReadOnly => ((IDictionary<KEY, VALUE>)KeyToValueMap).IsReadOnly;
 
         public void Add(KEY key, VALUE value)
         {
-            keyToValueMap.Add(key, value);
+            KeyToValueMap.Add(key, value);
         }
 
         public void Add(KeyValuePair<KEY, VALUE> item)
         {
-            ((IDictionary<KEY, VALUE>)keyToValueMap).Add(item);
+            ((IDictionary<KEY, VALUE>)KeyToValueMap).Add(item);
         }
 
         public void Clear()
         {
-            keyToValueMap.Clear();
+            KeyToValueMap.Clear();
         }
 
         public bool Contains(KeyValuePair<KEY, VALUE> item)
         {
-            return ((IDictionary<KEY, VALUE>)keyToValueMap).Contains(item);
+            return ((IDictionary<KEY, VALUE>)KeyToValueMap).Contains(item);
         }
 
         public bool ContainsKey(KEY key)
         {
-            return keyToValueMap.ContainsKey(key);
+            return KeyToValueMap.ContainsKey(key);
         }
 
         public void CopyTo(KeyValuePair<KEY, VALUE>[] array, int arrayIndex)
         {
-            ((IDictionary<KEY, VALUE>)keyToValueMap).CopyTo(array, arrayIndex);
+            ((IDictionary<KEY, VALUE>)KeyToValueMap).CopyTo(array, arrayIndex);
         }
 
         public IEnumerator<KeyValuePair<KEY, VALUE>> GetEnumerator()
         {
-            return ((IDictionary<KEY, VALUE>)keyToValueMap).GetEnumerator();
+            return ((IDictionary<KEY, VALUE>)KeyToValueMap).GetEnumerator();
         }
 
         public bool Remove(KEY key)
         {
-            return keyToValueMap.Remove(key);
+            return KeyToValueMap.Remove(key);
         }
 
         public bool Remove(KeyValuePair<KEY, VALUE> item)
         {
-            return ((IDictionary<KEY, VALUE>)keyToValueMap).Remove(item);
+            return ((IDictionary<KEY, VALUE>)KeyToValueMap).Remove(item);
         }
 
         public bool TryGetValue(KEY key, out VALUE value)
         {
-            return keyToValueMap.TryGetValue(key, out value);
+            return KeyToValueMap.TryGetValue(key, out value);
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return ((IDictionary<KEY, VALUE>)keyToValueMap).GetEnumerator();
+            return ((IDictionary<KEY, VALUE>)KeyToValueMap).GetEnumerator();
         }
     }
 }
