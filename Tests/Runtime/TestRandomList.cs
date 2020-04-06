6c157a8e2529ac6d7c4391960a05be299d19b0bd
Taro Omiya <taro.omiya@omiyagames.com>
2020-04-06 01:10:09 -0400

    Adding a template unit test script for every class that probably needs testings sooner rather than later.

---
+++ Tests/Runtime/TestRandomList.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace OmiyaGames.Common.Runtime.Tests
+{
+    ///-----------------------------------------------------------------------
+    /// <copyright file="TestRandomList.cs" company="Omiya Games">
+    /// The MIT License (MIT)
+    /// 
+    /// Copyright (c) 2020 Omiya Games
+    /// 
+    /// Permission is hereby granted, free of charge, to any person obtaining a copy
+    /// of this software and associated documentation files (the "Software"), to deal
+    /// in the Software without restriction, including without limitation the rights
+    /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    /// copies of the Software, and to permit persons to whom the Software is
+    /// furnished to do so, subject to the following conditions:
+    /// 
+    /// The above copyright notice and this permission notice shall be included in
+    /// all copies or substantial portions of the Software.
+    /// 
+    /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    /// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    /// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    /// THE SOFTWARE.
+    /// </copyright>
+    /// <author>Taro Omiya</author>
+    /// <date>4/6/2020</date>
+    ///-----------------------------------------------------------------------
+    /// <summary>
+    /// Unit test script for <see cref="RandomList{T}"/>.
+    /// </summary>
+    /// 
+    /// <seealso cref="RandomList{T}"/>
+    /// 
+    /// <remarks>
+    /// Revision History:
+    /// <list type="table">
+    /// <listheader>
+    ///   <description>Date</description>
+    ///   <description>Author</description>
+    ///   <description>Description</description>
+    /// </listheader>
+    /// <item>
+    ///   <description>4/6/2020</description>
+    ///   <description>Taro</description>
+    ///   <description>Initial verison</description>
+    /// </item>
+    /// </list>
+    /// </remarks>
+    public class TestRandomList
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void TestRandomListSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator TestRandomListWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
