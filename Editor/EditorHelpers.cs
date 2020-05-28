4bb894ff602a25d33671522eed1725db1ed2047f
Taro Omiya <taro.omiya@omiyagames.com>
2020-05-28 15:12:56 -0400

    Finally updating the last scripting documentation on Editor/EditorHelpers.cs

--- Editor/EditorHelpers.cs
+++ Editor/EditorHelpers.cs
@@ -73,39 +73,46 @@ namespace OmiyaGames.Common.Editor
     public static class EditorHelpers
     {
         /// <summary>
-        /// 
+        /// The minimum help box height on inspector.
         /// </summary>
         public const float MinHelpBoxHeight = 30f;
         /// <summary>
-        /// 
+        /// The default vertical margin between controls.
         /// </summary>
         public const float VerticalMargin = 2f;
         /// <summary>
-        /// 
+        /// Vertical gap between groups of controls to indicate separation.
         /// </summary>
         public const float VerticalSpace = 8f;
         /// <summary>
-        /// 
+        /// Amount of space to indent for embedded controls.
         /// </summary>
         public const float IndentSpace = 14f;
 
         /// <summary>
-        /// 
+        /// Single line height, plus the number of vertical margins
+        /// (usually between 0 to 2).
         /// </summary>
-        /// <param name="verticalMargin"></param>
-        /// <returns></returns>
+        /// <param name="verticalMargin">
+        /// Number of margins. 1 for top-only, 2 for top and bottom.
+        /// </param>
+        /// <returns>Vertical height for single line, plus margins.</returns>
         public static float SingleLineHeight(float verticalMargin)
         {
             return EditorGUIUtility.singleLineHeight + (verticalMargin * 2);
         }
 
         /// <summary>
-        /// 
+        /// Gets total height for an editor.
         /// </summary>
-        /// <param name="label"></param>
-        /// <param name="numRows"></param>
-        /// <param name="verticalMargin"></param>
-        /// <returns></returns>
+        /// <param name="label">
+        /// Label for the control: if it's displayed, increments number of rows.
+        /// </param>
+        /// <param name="numRows">Number of rows, e.g. number of controls.</param>
+        /// <param name="verticalMargin">
+        /// The height of each margin between rows.
+        /// </param>
+        /// <returns>Total height for editor.</returns>
         public static float GetHeight(GUIContent label, int numRows, float verticalMargin = VerticalMargin)
         {
             if ((label != null) && (string.IsNullOrEmpty(label.text) == false))
@@ -116,11 +123,13 @@ namespace OmiyaGames.Common.Editor
         }
 
         /// <summary>
-        /// 
+        /// Gets total height for an editor.
         /// </summary>
-        /// <param name="numRows"></param>
-        /// <param name="verticalMargin"></param>
-        /// <returns></returns>
+        /// <param name="numRows">Number of rows, e.g. number of controls.</param>
+        /// <param name="verticalMargin">
+        /// The height of each margin between rows.
+        /// </param>
+        /// <returns>Total height for editor.</returns>
         public static float GetHeight(int numRows, float verticalMargin = VerticalMargin)
         {
             float height = (EditorGUIUtility.singleLineHeight * numRows);
@@ -129,12 +138,14 @@ namespace OmiyaGames.Common.Editor
         }
 
         /// <summary>
-        /// 
+        /// Get the height of the help box, based on the text and width of controls.
         /// </summary>
-        /// <param name="text"></param>
-        /// <param name="viewWidth"></param>
-        /// <param name="minHeight"></param>
-        /// <returns></returns>
+        /// <param name="text">The text to display in the help box.</param>
+        /// <param name="viewWidth">Width of the view.</param>
+        /// <param name="minHeight">
+        /// The minimum height of help box, if text is short.
+        /// </param>
+        /// <returns>The height of the help box to display.</returns>
         public static float GetHelpBoxHeight(string text, float viewWidth, float minHeight = MinHelpBoxHeight)
         {
             var content = new GUIContent(text);
@@ -144,10 +155,16 @@ namespace OmiyaGames.Common.Editor
         }
 
         /// <summary>
-        /// 
+        /// Sets up an <see cref="AnimBool"/>.
         /// </summary>
-        /// <param name="editor"></param>
-        /// <param name="boolAnimation"></param>
+        /// <param name="editor">
+        /// The editor to add the <see cref="AnimBool"/> to.
+        /// </param>
+        /// <param name="boolAnimation">
+        /// The variable to set the new <see cref="AnimBool"/> to.
+        /// Note that if it's already set,
+        /// that variable will be destroyed first before creating a new one.
+        /// </param>
         public static void CreateBool(UnityEditor.Editor editor, ref AnimBool boolAnimation)
         {
             // Destroy the last animation, if any
@@ -159,10 +176,12 @@ namespace OmiyaGames.Common.Editor
         }
 
         /// <summary>
-        /// 
+        /// Destroys and cleans up <see cref="AnimBool"/>.
         /// </summary>
-        /// <param name="editor"></param>
-        /// <param name="boolAnimation"></param>
+        /// <param name="editor">
+        /// Editor to remove <see cref="AnimBool"/>'s events from.
+        /// </param>
+        /// <param name="boolAnimation">The <see cref="AnimBool"/> to clean-up.</param>
         public static void DestroyBool(UnityEditor.Editor editor, ref AnimBool boolAnimation)
         {
             if (boolAnimation != null)
@@ -175,9 +194,11 @@ namespace OmiyaGames.Common.Editor
         /// <summary>
         /// Helper method to draw enums from a limited range.
         /// </summary>
-        /// <typeparam name="ENUM"></typeparam>
-        /// <param name="property"></param>
-        /// <param name="supportedEnums">List of supported enums.  The first element is treated as default.</param>
+        /// <typeparam name="ENUM">Enum to draw in the editor.</typeparam>
+        /// <param name="property">Property being drawn.</param>
+        /// <param name="supportedEnums">
+        /// List of supported enums.  The first element is treated as default.
+        /// </param>
         public static void DrawEnum<ENUM>(SerializedProperty property, params ENUM[] supportedEnums) where ENUM : System.Enum
         {
             DrawEnum(property, supportedEnums, supportedEnums[0]);
@@ -186,15 +207,17 @@ namespace OmiyaGames.Common.Editor
         /// <summary>
         /// Helper method to draw enums from a limited range.
         /// </summary>
-        /// <typeparam name="ENUM"></typeparam>
-        /// <param name="property"></param>
-        /// <param name="supportedEnums">List of supported enums.  The first element is treated as default.</param>
+        /// <typeparam name="ENUM">Enum to draw in the editor.</typeparam>
+        /// <param name="property">Property being drawn.</param>
+        /// <param name="supportedEnums">
+        /// List of supported enums.  The first element is treated as default.
+        /// </param>
         public static void DrawEnum<ENUM>(SerializedProperty property, ENUM[] supportedEnums, ENUM defaultEnum) where ENUM : System.Enum
         {
             // Setup the pop-up
             string[] enumNames = new string[supportedEnums.Length];
             int[] enumValues = new int[supportedEnums.Length];
-            for(int index = 0; index < supportedEnums.Length; ++index)
+            for (int index = 0; index < supportedEnums.Length; ++index)
             {
                 enumNames[index] = ObjectNames.NicifyVariableName(supportedEnums[index].ToString());
                 enumValues[index] = Helpers.ConvertToInt(supportedEnums[index]);
@@ -212,7 +235,7 @@ namespace OmiyaGames.Common.Editor
 
             // Verify the selected value is within range
             ENUM selectedValue = Helpers.ConvertToEnum<ENUM>(property.enumValueIndex);
-            if(ArrayUtility.Contains(supportedEnums, selectedValue) == false)
+            if (ArrayUtility.Contains(supportedEnums, selectedValue) == false)
             {
                 // If not, select the default option
                 property.enumValueIndex = Helpers.ConvertToInt(defaultEnum);
@@ -223,11 +246,15 @@ namespace OmiyaGames.Common.Editor
         /// Helper method to draw enums from a limited range.
         /// Draws a warning if the target has an enum that doesn't match the property
         /// </summary>
-        /// <typeparam name="ENUM"></typeparam>
-        /// <param name="property"></param>
-        /// <param name="supportedEnums"></param>
-        /// <param name="targetsEnum"></param>
-        /// <param name="defaultEnum"></param>
+        /// <typeparam name="ENUM">Enum to draw in the editor.</typeparam>
+        /// <param name="property">Property being drawn.</param>
+        /// <param name="supportedEnums">
+        /// List of supported enums.  The first element is treated as default.
+        /// </param>
+        /// <param name="targetsEnum">
+        /// If an enum value is not supported, this enum is used instead.
+        /// </param>
+        /// <param name="defaultEnum">Default value to set the enum to.</param>
         public static void DrawEnum<ENUM>(SerializedProperty property, ENUM[] supportedEnums, ENUM defaultEnum, ENUM targetsEnum, string message = "\"{0}\" is not supported; \"{1}\" will be used instead.") where ENUM : System.Enum
         {
             // Check if we need to display the warning
@@ -246,10 +273,12 @@ namespace OmiyaGames.Common.Editor
         }
 
         /// <summary>
-        /// 
+        /// Draws a foldout with bold text.
         /// </summary>
-        /// <param name="buildSettingsAnimation"></param>
-        /// <param name="displayLabel"></param>
+        /// <param name="buildSettingsAnimation">
+        /// <see cref="AnimBool"/> for playing the slide-open animation.
+        /// </param>
+        /// <param name="displayLabel">Text to display on foldout.</param>
         public static void DrawBoldFoldout(AnimBool buildSettingsAnimation, string displayLabel)
         {
             // Grab foldout style
