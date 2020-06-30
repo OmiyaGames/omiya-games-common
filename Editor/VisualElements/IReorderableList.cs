af74011b875e3e1d10c499362ec029c817f76598
Taro Omiya <taro.omiya@omiyagames.com>
2020-06-30 17:06:14 -0400

    Working on adding new reorderable list VisualElements.

---
+++ Editor/VisualElements/IReorderableList.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace OmiyaGames.Common.Editor
+{
+    public abstract class IReorderableList<T> : VisualElement
+    {
+    }
+}
